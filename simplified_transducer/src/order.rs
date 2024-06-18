86821ef36f4d0dbb8ea2bdaaaa6b712a9eb50c6d
OjMaha <128642315+OjMaha@users.noreply.github.com>
2024-06-18 23:46:06 +0200

    Order_done

--- simplified_transducer/src/order.rs
+++ simplified_transducer/src/order.rs
@@ -1,35 +1,75 @@
-use simplified_transducer::ast::{Stmt, Pexpr, Bexpr};
-
-pub fn print_vec_string(vec: &Vec<String>) {
-    for i in 0..vec.len() {
-        print!("{}", vec[i]);
-        if i != vec.len() - 1 {
-            print!(", ");
-        }
-        else {
-            print!("\n");
-        }
-    }
-}
+use simplified_transducer::ast::Bexpr;
 
 pub fn generate_order_formula(
-    stmts: &[Stmt],
-    path: &mut Vec<usize>,
-    labels: &mut Vec<Vec<usize>>,
-    universe_formulas: &mut Vec<(Vec<String>, Bexpr)>,
+    universe_formulas: &[(Vec<String>, Bexpr)],
+    for0_or_for1: &[usize],
     order_formulas: &mut Vec<(usize, usize, Bexpr)>,
 ) {
-    //for each print statement pair accessible from universe_formula, print the vec<string> associated with it
-    for i in 0..universe_formulas.len() {
-        
-        let mut vec_i = universe_formulas[i].0.clone();
+    // Convert vector of vector of strings to vector of vector of i32
+    let for_vars: Vec<Vec<i32>> = universe_formulas.iter()
+        .map(|(vars, _)| vars.iter().map(|var| var[1..].parse::<i32>().unwrap()).collect())
+        .collect();
 
-        for j in i+1..universe_formulas.len() {
+    // Logic for generating order formulas
+    for i in 0..universe_formulas.len() {
+        for j in i + 1..universe_formulas.len() {
             
-            let mut vec_j = universe_formulas[j].0.clone();
+            //store the largest common prefix array for each of the arrays for_vars[i] and for_vars[j]
+            let mut lcp = Vec::new();
+            let min_len = std::cmp::min(for_vars[i].len(), for_vars[j].len());
+            for k in 0..min_len {
+                if for_vars[i][k] == for_vars[j][k] {
+                    lcp.push(for_vars[i][k]);
+                } else {
+                    break;
+                }
+            }
+
+            // Generate the order formula by iterating backwards through the LCP array
+            let mut order_formula = Bexpr::Var("T".to_string()); // Initial condition
+
+            for k in (0..lcp.len()).rev() {
+                let lhs = Bexpr::Var(format!("X{}", lcp[k])).clone(); // Cloning to avoid move
+                let rhs = Bexpr::Var(format!("x{}", lcp[k])).clone(); // Cloning to avoid move
+
+                let less = Bexpr::Less(Box::new(lhs.clone()), Box::new(rhs.clone()));
+                let greater = Bexpr::Greater(Box::new(lhs.clone()), Box::new(rhs.clone()));
+                let greater_equal = Bexpr::GreaterEqual(Box::new(lhs.clone()), Box::new(rhs.clone()));
+                let less_equal = Bexpr::LessEqual(Box::new(lhs.clone()), Box::new(rhs.clone()));
+                let equal = Bexpr::Equal(Box::new(lhs), Box::new(rhs));
+
+                let combined_condition = if k == lcp.len() - 1 {
+                    // Last element of LCP: combine with just the less condition
+                    if for0_or_for1[k] == 0 {
+                        less_equal
+                    }
+
+                    else {
+                        greater_equal
+                    }
+                    
+                } else {
+                    // Not the last element: combine with the OR of less and equal conditions
+                    if for0_or_for1[k] == 0 {
+                        Bexpr::Or(Box::new(less), Box::new(Bexpr::And(Box::new(equal), Box::new(order_formula.clone()))))
+                    }
+
+                    else {
+                        Bexpr::Or(Box::new(greater), Box::new(Bexpr::And(Box::new(equal), Box::new(order_formula.clone()))))
+                    }
+                };
+
+                order_formula = combined_condition;
+            }
+
+            // Now order_formula contains the complete nested order conditions
+
+
+            order_formulas.push((i, j, order_formula));
             
-            println!("Comparing {i}: {:?} and {j}: {:?}", vec_i, vec_j);
-            
+
         }
     }
 }
+
+
